40c82dc2d16e7288741077b55c5403b4cfdc81a7
Mark Harder <mark.harder899@gmail.com>
2016-02-20 14:04:25 +0900

    add room 1 output

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  char input;
+
+  // Room 1
+  cout << "You see a small, silver dagger on the floor." << endl;
+  cout << "1. Take it" << endl;
+  cout << "2. Leave it" << endl;
+  cout << "> ";
+
+  cin >> input;
+
+  if (input == '1') {
+    cout << "You take the silver dagger and move on to the next room." << endl;
+  } else {
+    cout << "You walk past the dagger to the next room." << endl;
+  }
+
+  // Room 2
+  cout << "Room two description." << endl;
 
   return 0;
 }
