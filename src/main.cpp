30bc50a876dba67e40b49b11a95a3a60a459b263
Mark Harder <mark.harder899@gmail.com>
2016-02-22 11:42:59 +0900

    add rooms 3 and 4

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ int main() {
 
   if (input == '1') {
     cout << "You take the silver dagger and move on to the next room." << endl;
-    player.addItem( "dagger" );
+    player.addItem("dagger");
   } else {
     cout << "You walk past the dagger to the next room." << endl;
   }
@@ -35,15 +35,15 @@ int main() {
   // Room 2
   printItems(player.getItems());
   cout << "You see a glass globe on a plinth." << endl;
-  cout << "1> Take it" << endl;
+  cout << "1. Take it" << endl;
   cout << "2. Leave it" << endl;
   cout << "> ";
 
   cin >> input;
 
   if (input == '1') {
-    cout << "As you touch the glass, it explodes into many sharp shards." << endl;
-    player.addPoints(-8);
+    cout << "As you touch the glass, it explodes into many sharp shards. You go on." << endl;
+    player.addPoints(-3);
   } else {
     cout << "You walk past the orb to the next room." << endl;
   }
@@ -51,7 +51,44 @@ int main() {
   // Room 3
   printItems(player.getItems());
   cout << "Score: " << player.getPoints() << endl;
-  cout << "Room 3 description" << endl;
+  cout << "You see a small statue of a fat monk resting on a red cushion." << endl;
+  cout << "1. Take it" << endl;
+  cout << "2. Leave it" << endl;
+  cout << "> ";
+
+  cin >> input;
+
+  if (input == '1') {
+    cout << "You take the statue." << endl;
+    player.addItem("statue");
+  } else {
+    cout << "You leave the statue." << endl;
+  }
+
+  // Room 4
+  printItems(player.getItems());
+  cout << "Score: " << player.getPoints() << endl;
+  cout << "You see three blue imps climbing near a window." << endl;
+  cout << "1. Take them" << endl;
+  cout << "2. Leave them" << endl;
+  cout << "3. Talk to them" << endl;
+  cout << "> ";
+
+  cin >> input;
+
+  if (input == '1') {
+    cout << "The imps are not happy to see you try to take them." << endl;
+    player.addPoints(-8);
+  } else if (input =='2') {
+    cout << "You walk past them." << endl;
+  } else {
+    cout << "You try to talk to them, but they just giggle." << endl;
+  }
+
+  // Room 5
+  printItems(player.getItems());
+  cout << "Score: " << player.getPoints() << endl;
+  cout << "Room 5" << endl;
 
   return 0;
 }
